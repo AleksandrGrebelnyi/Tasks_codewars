5e26378aa47a963507aff18c209e8830a68226c4
Aleksandr <aleksandr955@gmail.com>
2022-05-26 11:31:43 +0300

    Create function which determined first non-consecutive char

---
+++ first_non_consecutive.py
@@ -0,0 +1,13 @@
+def first_non_consecutive(arr):
+    char = arr[0]
+    for i in arr:
+        if i != char:
+            return i
+        char += 1
+    return None
+
+sp = [1, 2, 3, 4, 6, 7, 8]
+print(first_non_consecutive(sp))
+
+
+
